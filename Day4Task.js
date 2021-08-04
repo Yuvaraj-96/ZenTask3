8d9a2db9608763e84c0a3e98d5acfd839becb387
Yuvaraj <87924193+Yuvaraj-96@users.noreply.github.com>
2021-08-04 23:13:36 +0530

    Update File.

--- Day4Task.js
+++ Day4Task.js
@@ -44,9 +44,3 @@ function outPut(data) {
   
 }
 
-// let url = 'https://restcountries.eu/rest/v2/all';
-
-// fetch(url).then(res => res.json()).then((out) => {
-//   console.log('Checkout this JSON! ', out);
-// })
-// .catch(err => { throw err });
