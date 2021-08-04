ed77aa6126238faeca3ab2322c41ca512e66055d
Yuvaraj <87924193+Yuvaraj-96@users.noreply.github.com>
2021-08-04 23:12:54 +0530

    Uploading the Day 4 Task.

---
+++ Day4Task.js
@@ -0,0 +1,52 @@
+
+const fetch = require("node-fetch");
+var obj1 = { name: "Person 1", age:5 , Year:2018};
+var obj2 = { age:5, name: "Person 1",  Year:2020};
+//Task 1
+for(let x in obj1)
+{
+    for(let y in obj2)
+    {
+        if(x == y)
+        {
+            if(obj1[x] == obj2[y])
+        {
+            console.log(`Values are same in the json files Value is ${obj1[x]} and Key is ${x}.`)
+
+        }else{
+            console.log(`Keys are same but values are not same. `);
+        }
+
+        }else{
+           // console.log(`Keys are not same. `);
+            continue;
+        }
+    }
+}
+//Task 3
+fetch('https://restcountries.eu/rest/v2/all').then(response => response.json()).then(data => outPut(data)).catch(error => console.log("Error:", error.message));
+
+function outPut(data) {
+//let count =0;
+    for(let x of data)
+    {
+        //if(count<=50){
+        console.log(`Name ${x.name};`);
+        console.log(`Region ${x.region};`);
+        console.log(`Sub Region ${x.subregion};`);
+        console.log(`Population ${x.population};`); 
+        console.log(`Flag ${x.flag};`);        
+        //count++;
+        //}
+
+
+    }
+  
+}
+
+// let url = 'https://restcountries.eu/rest/v2/all';
+
+// fetch(url).then(res => res.json()).then((out) => {
+//   console.log('Checkout this JSON! ', out);
+// })
+// .catch(err => { throw err });
